d337b3b41a00801edfdce0144bb5279c2fe2e225
Mitul Hirna <mitul.hirna@publicissapient.com>
2023-02-24 00:29:23 +0530

    fixes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,21 +2,21 @@ import React from 'react'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
-const generateClassName = new createGenerateClassName({
+const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
 });
 
 export default () => {
     return (
-        <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
                     <MarketingApp />
                 </div>
-            </BrowserRouter>
-        </StylesProvider>
+            </StylesProvider>
+        </BrowserRouter>
     );
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 import Landing from './components/Landing'
 import Pricing from "./components/Pricing";
-const generateClassName = new createGenerateClassName({
+
+const generateClassName = createGenerateClassName({
     productionPrefix: 'ma',
 });
+
 export default () => {
     return <div>
-        <StylesProvider generateClassName={generateClassName}>
-            <BrowserRouter>
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
                 <Switch>
                     <Route exact path="/pricing" component={Pricing} />
                     <Route path="/" component={Landing} />
                 </Switch>
-            </BrowserRouter>
-        </StylesProvider>
+            </StylesProvider>
+        </BrowserRouter>
     </div>
 }
 
